232dc4c0686285f0cb0da5e18af19e0cd5a0c3d8
flippedcoder <milecia.mcgregor@aol.com>
2022-04-14 14:36:09 -0500

    formatted inventory code

--- inventory-tracker/pages/index.tsx
+++ inventory-tracker/pages/index.tsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
-import axios from 'axios'
-import Image from 'next/image'
-import { PlusCircleIcon, PencilAltIcon, TrashIcon } from '@heroicons/react/solid'
-import Modal from '../components/Modal'
+import { useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import {
+  PlusCircleIcon,
+  PencilAltIcon,
+  TrashIcon,
+} from "@heroicons/react/solid";
+import Modal from "../components/Modal";
 
 interface Product {
-  id: string
-  name: string
-  sku: string
-  image: string
-  quantity: number
-  storeId: string
+  id: string;
+  name: string;
+  sku: string;
+  image: string;
+  quantity: number;
+  storeId: string;
 }
 
 export default function Products({ products }) {
-  const [newModal, setNewModal] = useState<boolean>(false)
-  const [editModal, setEditModal] = useState<boolean>(false)
-  const [index, setIndex] = useState<number>(0)
+  const [newModal, setNewModal] = useState<boolean>(false);
+  const [editModal, setEditModal] = useState<boolean>(false);
+  const [index, setIndex] = useState<number>(0);
 
   const deleteProduct = async (productId: string) => {
-    await axios.delete('/api/inventory?type=delete', { data: { productId } })
-  }
+    await axios.delete("/api/inventory?type=delete", { data: { productId } });
+  };
 
   return (
     <div>
@@ -31,36 +35,53 @@ export default function Products({ products }) {
         </div>
       </div>
       <div className="flex flex-col gap-2">
-        {products.length > 0 ? products.map((product: Product, index: number) => (
-          <div key={product.id} className="flex gap-6 border-b-2 w-full">
-            <Image src={product.image} alt={product.name} width="100" height="100" />
-            <div>{product.name}</div>
-            <div>{product.quantity}</div>
-            <div>{product.storeId}</div>
-            <PencilAltIcon className="h-6 w-6 text-indigo-500" onClick={() => {
-              setIndex(index)
-              setEditModal(true)
-            }} />
-            <TrashIcon className="h-6 w-6 text-red-400" onClick={() => deleteProduct(product.id)} />
-          </div>
-        )) :
+        {products.length > 0 ? (
+          products.map((product: Product, index: number) => (
+            <div key={product.id} className="flex gap-6 border-b-2 w-full">
+              <Image
+                src={product.image}
+                alt={product.name}
+                width="100"
+                height="100"
+              />
+              <div>{product.name}</div>
+              <div>{product.quantity}</div>
+              <div>{product.storeId}</div>
+              <PencilAltIcon
+                className="h-6 w-6 text-indigo-500"
+                onClick={() => {
+                  setIndex(index);
+                  setEditModal(true);
+                }}
+              />
+              <TrashIcon
+                className="h-6 w-6 text-red-400"
+                onClick={() => deleteProduct(product.id)}
+              />
+            </div>
+          ))
+        ) : (
           <div>Add some new products</div>
-        }
+        )}
       </div>
       {newModal && <Modal product={{}} onCancel={() => setNewModal(false)} />}
-      {editModal && <Modal product={products[index]} onCancel={() => setEditModal(false)} />}
+      {editModal && (
+        <Modal product={products[index]} onCancel={() => setEditModal(false)} />
+      )}
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-  const productsRes = await fetch('http://localhost:3000/api/inventory?type=products')
+  const productsRes = await fetch(
+    "http://localhost:3000/api/inventory?type=products"
+  );
 
-  const products = await productsRes.json()
+  const products = await productsRes.json();
 
   return {
     props: {
-      products
+      products,
     },
-  }
+  };
 }
